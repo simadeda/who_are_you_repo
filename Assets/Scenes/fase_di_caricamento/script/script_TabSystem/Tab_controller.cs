d5a6217bf9544b6265df4623205c2137912b9eb2
simadeda <simadedarav@gmail.com>
2021-05-21 14:55:52 +0200

    aggiunti i commenti ai 2 script nuovi

--- Assets/Scenes/fase_di_caricamento/script/script_TabSystem/Tab_controller.cs
+++ Assets/Scenes/fase_di_caricamento/script/script_TabSystem/Tab_controller.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public class Tab_controller : MonoBehaviour
 {
-    public List<Tab_bottone> Tab_bottoni = new List<Tab_bottone>();
-    public List<GameObject> Tab_pannello; 
+    public List<Tab_bottone> Tab_bottoni = new List<Tab_bottone>(); //lista di gameobj che hanno al loro interno lo script "Tab_bottone" (i bottoni) o almeno quesso è quello che so capito
+    public List<GameObject> Tab_pannello; //lista di gameobj per il pannello
            
-    public void Tab_enter(Tab_bottone Bottone)
+    public void Tab_enter(Tab_bottone Bottone) //funzione per il click deju mouse
     {
-        int indice = Bottone.transform.GetSiblingIndex();
-        for(int i=0; i<Tab_pannello.Count; i++)
+        int indice = Bottone.transform.GetSiblingIndex(); //pezzo ancora da capi bene
+        for(int i=0; i<Tab_pannello.Count; i++) //for pe seleziona il pannello giusto in base al bottone cliccato
         {
             if(i == indice)
             {
@@ -22,7 +22,7 @@ public class Tab_controller : MonoBehaviour
         }
     }
 
-    public void Tab_exit(Tab_bottone Bottone)
+    public void Tab_exit(Tab_bottone Bottone)  
     {
 
     }
