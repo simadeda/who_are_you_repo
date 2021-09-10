1be2fa7ab339107b74e25077ccf1be19c19556dd
simadeda <simadedarav@gmail.com>
2021-09-10 12:14:42 +0200

    aggiunta paletta e ground per mappa1

--- Assets/Scenes/fase_di_caricamento/script/script_TabSystem/Tab_bottone.cs
+++ Assets/Scenes/fase_di_caricamento/script/script_TabSystem/Tab_bottone.cs
@@ -4,13 +4,18 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems; //"libreria" per gli event system
 using UnityEngine;
 
-
+[RequireComponent(typeof(Image))]
 public class Tab_bottone : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler 
 {
-   public Tab_controller Tab_controller; //riferimento allo script Tab_controller
+    public Tab_controller Tab_controller; //riferimento allo script Tab_controller
+    public List<Tab_bottone> tab_bottoni;
+    public Sprite btn_inattivo;
+    public Image img_icona_sfondo;
 
-    //public Image BackgroundIMG;
-
+    void Start()
+    {
+        img_icona_sfondo = GetComponent<Image>();
+    }
     public void OnPointerClick(PointerEventData eventData) //quando clicchi entra entro a sta funzione
     {
         Tab_controller.Tab_enter(this); //per ora va solo questo
@@ -24,6 +29,14 @@ public class Tab_bottone : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     public void OnPointerExit(PointerEventData eventData) //quando passando sopre co ju mouse e poi escire dal bottone senza cliccarlo entra ecco
     {
         Tab_controller.Tab_exit(this);
+    }
+
+    public void img_icona_reset()
+    {
+        foreach(Tab_bottone bottone in tab_bottoni)
+        {
+            bottone.img_icona_sfondo.sprite = btn_inattivo;
+        }
     }
 
     
