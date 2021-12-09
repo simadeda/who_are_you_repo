7bc7b84473e237e0de1071b667517a2b0b7f7f24
simadeda <simadedarav@gmail.com>
2021-12-09 11:51:18 +0100

    animazioni destra sinistra

--- Assets/Scenes/game_map_1.0/Script_game_map_1/Movimenti_player/Movimenti_player.cs
+++ Assets/Scenes/game_map_1.0/Script_game_map_1/Movimenti_player/Movimenti_player.cs
@@ -8,13 +8,20 @@ public class Movimenti_player : MonoBehaviour
     float velocita = 15f;
     Vector2 direzione = new Vector2(0, 0);
     public CharacterController controlli;
+    public Animator animator;
    
     void FixedUpdate()
     {
        direzione.x = Input.GetAxisRaw("Horizontal") * velocita * Time.fixedDeltaTime;
-       transform.Translate(direzione * velocita * Time.fixedDeltaTime);
+       transform.Translate(direzione * velocita *  Time.fixedDeltaTime);
+       animator.SetFloat("verticale", direzione.y);
 
        direzione.y = Input.GetAxisRaw("Vertical") * velocita * Time.fixedDeltaTime;
        transform.Translate(direzione * velocita * Time.fixedDeltaTime);
+       animator.SetFloat("orizzontale", direzione.x);
+
+       animator.SetFloat("velocità",direzione.sqrMagnitude);
+         
+       
     }
 }
