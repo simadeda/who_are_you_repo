d28e6a6b4f3a3701c54017e696155cdd0e910928
simadeda <simadedarav@gmail.com>
2022-01-22 18:07:56 +0100

    aggiunte animazione idle xs,dx,avanti,dietro

--- Assets/Scenes/game_map_1.0/Script_game_map_1/Movimenti_player/Movimenti_player.cs
+++ Assets/Scenes/game_map_1.0/Script_game_map_1/Movimenti_player/Movimenti_player.cs
@@ -6,6 +6,8 @@ public class Movimenti_player : MonoBehaviour
 {
     [SerializeField]
     float velocita = 15f;
+    static float sx_dx_controller = 0;
+    static float dietro_avanti_controller = 0;
     Vector2 direzione = new Vector2(0, 0);
     public CharacterController controlli;
     public Animator animator;
@@ -15,13 +17,36 @@ public class Movimenti_player : MonoBehaviour
        direzione.x = Input.GetAxisRaw("Horizontal") * velocita * Time.fixedDeltaTime;
        transform.Translate(direzione * velocita *  Time.fixedDeltaTime);
        animator.SetFloat("verticale", direzione.y);
+       dietro_avanti_controller = direzione.y;
+       Idle();
+
 
        direzione.y = Input.GetAxisRaw("Vertical") * velocita * Time.fixedDeltaTime;
        transform.Translate(direzione * velocita * Time.fixedDeltaTime);
        animator.SetFloat("orizzontale", direzione.x);
+       sx_dx_controller = direzione.x;
+       Idle();
 
        animator.SetFloat("velocità",direzione.sqrMagnitude);
 
     }
-       
+
+    void Idle()
+    {
+        if(dietro_avanti_controller != 0)
+        {
+            animator.SetFloat("idle_dietro_avanti", dietro_avanti_controller);
+            sx_dx_controller = 0;
+            animator.SetFloat("idle_sx_dx", sx_dx_controller);
+        }
+
+        if(sx_dx_controller != 0)
+        {
+            animator.SetFloat("idle_sx_dx", sx_dx_controller);
+            dietro_avanti_controller = 0;
+            animator.SetFloat("idle_dietro_avanti", dietro_avanti_controller);
+        }
+            
+        
+    }
 }
