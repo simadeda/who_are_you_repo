22b572ab9a5b2407e5ec9d453dae52beba54093d
simadeda <simadedarav@gmail.com>
2022-09-19 21:41:40 +0200

    creazione script classi

---
+++ Assets/Scenes/game_map_1.0/Script_game_map_1/Script_classi/Classe_armaiolo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classe_armaiolo : Base_classi
+{
+    public void Armaiolo()
+    {
+        int vita_armaiolo = 150;
+    }
+}
