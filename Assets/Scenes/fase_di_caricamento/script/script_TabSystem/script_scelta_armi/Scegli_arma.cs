c2ccaa73c2fb1183e2e7f3623610ebce32f1dc51
simadeda <simadedarav@gmail.com>
2022-09-24 19:14:58 +0200

    correzioni varie & selezione arma ora disponibile

--- Assets/Scenes/fase_di_caricamento/script/script_TabSystem/script_scelta_armi/Scegli_arma.cs
+++ Assets/Scenes/fase_di_caricamento/script/script_TabSystem/script_scelta_armi/Scegli_arma.cs
@@ -5,12 +5,14 @@ using TMPro;
 
 public class Scegli_arma : MonoBehaviour
 {
-    public GameObject txt_arma_scelta, txt_descrizione_arma, txt_armi_utilizzabili,LettoreXMLgb;
+    public GameObject txt_arma_scelta, txt_descrizione_arma, txt_armi_utilizzabili, LettoreXMLgb, selezione_c96, selezione_g17;
     LettoreXML LettoreXMLscript;
     private TextMeshProUGUI TextMesH_arma_scelta, TextMesH_descrizione_arma, TextMesH_armi_utilizzabili;
 
-    private string[] nome_arma = new string[2];
-    private string[] descrizione_arma = new string[2];
+    private string[] nome_arma = new string[2], descrizione_arma = new string[2];
+    string armi_utilizzabili, arma_selezionata = "G17";
+
+
     void Start()
     {
         TextMesH_arma_scelta = txt_arma_scelta.GetComponent<TextMeshProUGUI>();
@@ -19,16 +21,36 @@ public class Scegli_arma : MonoBehaviour
         LettoreXMLscript= LettoreXMLgb.GetComponent<LettoreXML>();
 
         (nome_arma, descrizione_arma) = LettoreXMLscript.Lettura_armi(nome_arma, descrizione_arma);
-        string armi_utilizzabili = LettoreXML.Armi_utilizzabili;
-        armi_utilizzabili_classe(armi_utilizzabili,nome_arma,descrizione_arma);
-        
-    }
+        armi_utilizzabili = LettoreXML.Armi_utilizzabili;
 
-    void armi_utilizzabili_classe(string armi_utilizzabili, string[] nome_arma, string[] descrizione_arma)
-    {
         TextMesH_armi_utilizzabili.text = armi_utilizzabili;
         TextMesH_arma_scelta.text = nome_arma[0];
+        TextMesH_descrizione_arma.text = descrizione_arma[0];
+    }
 
+    public void armi_utilizzabili_classe(string arma_premuta)
+    {
+        if(arma_selezionata != arma_premuta)
+        {
+            switch(arma_premuta)
+            {
+                case "G17":
+                    TextMesH_arma_scelta.text = nome_arma[0];
+                    TextMesH_descrizione_arma.text = descrizione_arma[0];
+                    selezione_g17.SetActive(true);
+                    selezione_c96.SetActive(false);
+                    arma_selezionata = "G17";
+                    break;
+
+                case "C96":
+                    TextMesH_arma_scelta.text = nome_arma[1];
+                    TextMesH_descrizione_arma.text = descrizione_arma[1];
+                    selezione_c96.SetActive(true);
+                    selezione_g17.SetActive(false);
+                    arma_selezionata = "C96";
+                    break;
+            }
+        }
     }
    
 }
