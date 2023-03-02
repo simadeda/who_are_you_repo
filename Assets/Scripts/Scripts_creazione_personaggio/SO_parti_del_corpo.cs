3bd9e2201707db1061ea3148df322d6ad2f060ec
simadeda <simadedarav@gmail.com>
2023-03-02 12:41:57 +0100

    cambiamenti vari

--- Assets/Scripts/Scripts_creazione_personaggio/SO_parti_del_corpo.cs
+++ Assets/Scripts/Scripts_creazione_personaggio/SO_parti_del_corpo.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-
 //codice scrittoda da tutmo (youtube.com/tutmo) 
 //reciclato da daniele iapadre per who are you
-
 [CreateAssetMenu(fileName = "parte del corpo", menuName = "Parte del corpo")]
 public class SO_parti_del_corpo : ScriptableObject
 {
